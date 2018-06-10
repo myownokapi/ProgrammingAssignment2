691991475fd977a08e558b60959037bfe3e89ff0
myownokapi <39533935+myownokapi@users.noreply.github.com>
2018-06-10 17:31:53 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function cretes an empty matrix
+## This function creates an empty matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 		s <- NULL
